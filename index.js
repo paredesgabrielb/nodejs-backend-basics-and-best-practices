4a0d121800ad19482892b294a259900c1bfc99aa
Gabriel Paredes Abreu <gparedes@digepres.gob.do>
2023-06-02 16:17:23 -0400

    Middleware Layer

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const userRouter = require('./routes/userController');
+const helloMiddleware = require('./middleware/helloMiddleware');
 
+
+app.use(helloMiddleware);
 app.use(express.json());
 app.listen('3000');
 
--- routes/userController.js
+++ routes/userController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router  = express.Router();
 const findById = require('../services/userServices');
 
+
 // GET: /users
 router.get("/users",(req,res) =>{
     res.send('Users ');
